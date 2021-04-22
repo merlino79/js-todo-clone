3ce2392ef658c403f8054f3c74b330d67eeb70c6
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-22 20:09:02 +0200

    scrittura a video di una parloa piu lunga di tre lettere

--- asset/js/script.js
+++ asset/js/script.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
         "studiare",
         "seguire le mie passioni",
         "leggere",
+        "documentarmi"
 
     ];
 
@@ -14,25 +15,46 @@ $(document).ready(function() {
 
     for (var i = 0; i < arrTodo.length; i++) {
         var strTodo = arrTodo[i];
-        console.log(strTodo);
+        //console.log(strTodo);
 
         //     var strHtml = '<li><p>' + strTodo + '</p> <i class="fas fa-trash-alt"></i> </li>';
-        //     $('.app ul').append(strHtml); 
+
         //stampo a video
 
         var item = $('.template li').clone();
         $(item).find('.text').append(strTodo);
 
-
-
-
-
-
+        $('.app ul').append(item);
 
     }
 
-    //fine prima soluzione
-    // soluzione due con js
+
+
+
+    //ricarico la pagina del dom//
+    $(document).on('click', '.fa-trash-alt', function() {
+
+        console.log(this); //sto puntando l'oggetto al riavvio del dom
+        $(this).parent().remove();
+
+
+    });
+
+    $('.app input').keyup(function(event) {
+        console.log(event.which); //intercetto i tasti scritti //il tasto invio corrisponde al 13
+        if (event.which === 13) {
+            console.log($('.app input').val()); //stampo alla consol.log l'imput inserito
+            if ($(this).val().length > 3) {
+                //console.log($(this).val());
+                var item = $('.template li').clone();
+                $(item).find('.text').append($(this).val());
+
+                $('.app ul').append($(item));
+
+            }
+        }
+    });
+
 
 
 
@@ -52,4 +74,4 @@ $(document).ready(function() {
 
 
 });
-console.log("#tey")
+//console.log("#tey")
