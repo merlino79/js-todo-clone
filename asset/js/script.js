1e0628521fcf54d5d3db86b1c90864ef1c55a974
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-22 16:55:58 +0200

    studio

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,5 +1,33 @@
-$(function(){
+$(document).ready(function() {
+    //console.log('DOM');
 
-	
+    var arrTodo = [
+        "suonare",
+        "disegnare",
+        "studiare",
+        "seguire le mie passioni",
+        "leggere",
+
+    ];
+
+    console.log(arrTodo[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
+console.log("#tey")
