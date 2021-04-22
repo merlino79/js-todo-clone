5a67c4c1fe7471d93cad9042847fa49b3ed78feb
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-22 17:15:46 +0200

    se non depenni l'html non sovrascrivi la lista

--- asset/js/script.js
+++ asset/js/script.js
@@ -10,7 +10,21 @@ $(document).ready(function() {
 
     ];
 
-    console.log(arrTodo[0]);
+
+    for (var i = 0; i < arrTodo.length; i++) {
+        var strTodo = arrTodo[i];
+        console.log(strTodo);
+
+        var strHtml = '<li><p>' + strTodo + '</p> <i class="fas fa-trash-alt"></i> </li>';
+        $('.app ul').append(strHtml); //stampo a video
+
+
+
+
+
+
+
+    }
 
 
 
